25cd46163ea2878a26b0cabc17f1ccb44a7e43e2
Akilan <me@akilan.io>
2021-04-02 15:44:48 +0400

    added broadcast module for GPU specs

---
+++ server/gpu_test.go
@@ -0,0 +1,16 @@
+package server
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGpuOutput(t *testing.T) {
+	gpu, err := GPUInfo()
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	fmt.Print(gpu.Gpu.GpuName)
+}
