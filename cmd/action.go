977260dcf7cd559919bdc715bdba2a8e332e2b59
Akilan <me@akilan.io>
2021-04-14 21:20:33 +0400

    addeded 2 extra cli commands: ListServers and UpdateIPTableList

--- cmd/action.go
+++ cmd/action.go
@@ -16,13 +16,17 @@ var CliAction = func(ctx *cli.Context) error {
 	}
 
 	//Listing servers and also updates IP tables (Default 3 hops)
-	if ListServers {
+	if UpdateServerList {
 
 		err := client.UpdateIpTableListClient()
 		if err != nil {
 			fmt.Print(err)
 		}
 
+		p2p.PrintIpTable()
+	}
+
+	if ServerList {
 		p2p.PrintIpTable()
 	}
 
