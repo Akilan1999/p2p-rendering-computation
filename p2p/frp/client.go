69fb11ffd2e5a8d4e4d87b8d5a4c57449394030c
Akilan <me@akilan.io>
2022-12-29 03:17:42 +0000

    base version working for reverse proxy

--- p2p/frp/client.go
+++ p2p/frp/client.go
@@ -1,134 +1,140 @@
 package frp
 
 import (
-    "git.sr.ht/~akilan1999/p2p-rendering-computation/server/docker"
-    "github.com/fatedier/frp/client"
-    "github.com/fatedier/frp/pkg/config"
-    "math/rand"
-    "strconv"
+	"git.sr.ht/~akilan1999/p2p-rendering-computation/server/docker"
+	"github.com/fatedier/frp/client"
+	"github.com/fatedier/frp/pkg/config"
+	"math/rand"
+	"strconv"
 )
 
 // Client This struct stores
 // client information with server
 // proxy connected
 type Client struct {
-    Name           string
-    Server         *Server
-    ClientMappings []ClientMapping
+	Name           string
+	Server         *Server
+	ClientMappings []ClientMapping
 }
 
 // ClientMapping Stores client mapping ports
 // to proxy server
 type ClientMapping struct {
-    LocalIP    string
-    LocalPort  int
-    RemotePort int
+	LocalIP    string
+	LocalPort  int
+	RemotePort int
 }
 
 // StartFRPClientForServer Starts Server using FRP server
 // returns back a port
-func StartFRPClientForServer(ipaddress string, port string) (string, error) {
-    // Setup server information
-    var s Server
-    s.address = ipaddress
-    // convert port to int
-    portInt, err := strconv.Atoi(port)
-    if err != nil {
-        return "", err
-    }
-    s.port = portInt
+func StartFRPClientForServer(ipaddress string, port string, localport string) (string, error) {
+	// Setup server information
+	var s Server
+	s.address = ipaddress
+	// convert port to int
+	portInt, err := strconv.Atoi(port)
+	if err != nil {
+		return "", err
+	}
+	s.port = portInt
 
-    // Setup client information
-    var c Client
-    c.Name = "ServerPort"
-    c.Server = &s
+	// Setup client information
+	var c Client
+	c.Name = "ServerPort"
+	c.Server = &s
 
-    //random port
-    randPort := rangeIn(10000, 99999)
-    c.ClientMappings = []ClientMapping{
-        {
-            LocalIP:    ipaddress,
-            LocalPort:  randPort,
-            RemotePort: randPort,
-        },
-    }
+	// converts localport to int
+	portInt, err = strconv.Atoi(localport)
+	if err != nil {
+		return "", err
+	}
 
-    // Start client server
-    go c.StartFRPClient()
+	//random port
+	randPort := rangeIn(10000, 99999)
+	c.ClientMappings = []ClientMapping{
+		{
+			LocalIP:    ipaddress,
+			LocalPort:  portInt,
+			RemotePort: randPort,
+		},
+	}
 
-    return strconv.Itoa(randPort), nil
+	// Start client server
+	go c.StartFRPClient()
+
+	return strconv.Itoa(randPort), nil
 
 }
 
 func StartFRPCDockerContainer(ipaddress string, port string, docker *docker.DockerVM) error {
-    // Setup server information
-    var s Server
-    s.address = ipaddress
-    // convert port to int
-    portInt, err := strconv.Atoi(port)
-    if err != nil {
-        return err
-    }
-    s.port = portInt
+	// Setup server information
+	var s Server
+	s.address = ipaddress
+	// convert port to int
+	portInt, err := strconv.Atoi(port)
+	if err != nil {
+		return err
+	}
+	s.port = portInt
 
-    // Setup client information
-    var c Client
-    c.Name = "ServerPort"
-    c.Server = &s
+	// Setup client information
+	var c Client
+	c.Name = "ServerPort"
+	c.Server = &s
 
-    // set client mapping
-    var clientMappings []ClientMapping
-    for i, _ := range docker.Ports.PortSet {
-        // Set client mapping
-        var clientMapping ClientMapping
-        portMap := docker.Ports.PortSet[i].ExternalPort
-        clientMapping.LocalPort = portMap
-        clientMapping.RemotePort = portMap
-        // Append to array
-        clientMappings = append(clientMappings, clientMapping)
-    }
+	// set client mapping
+	var clientMappings []ClientMapping
+	for i, _ := range docker.Ports.PortSet {
+		// Set client mapping
+		var clientMapping ClientMapping
+		portMap := docker.Ports.PortSet[i].ExternalPort
+		clientMapping.LocalPort = portMap
+		clientMapping.RemotePort = portMap
+		// Append to array
+		clientMappings = append(clientMappings, clientMapping)
+	}
 
-    // Start client server
-    go c.StartFRPClient()
+	// Start client server
+	go c.StartFRPClient()
 
-    return nil
+	return nil
 
 }
 
 // StartFRPClient Starts FRP client
 func (c *Client) StartFRPClient() error {
 
-    cfg := config.GetDefaultClientConf()
+	cfg := config.GetDefaultClientConf()
 
-    var proxyConfs map[string]config.ProxyConf
-    var visitorCfgs map[string]config.VisitorConf
+	var proxyConfs map[string]config.ProxyConf
+	var visitorCfgs map[string]config.VisitorConf
 
-    proxyConfs = make(map[string]config.ProxyConf)
+	proxyConfs = make(map[string]config.ProxyConf)
 
-    cfg.ServerAddr = c.Server.address
-    cfg.ServerPort = c.Server.port
+	cfg.ServerAddr = c.Server.address
+	cfg.ServerPort = c.Server.port
 
-    for i, _ := range c.ClientMappings {
-        var tcpcnf config.TCPProxyConf
-        tcpcnf.LocalIP = c.ClientMappings[i].LocalIP
-        tcpcnf.LocalPort = c.ClientMappings[i].LocalPort
-        tcpcnf.RemotePort = c.ClientMappings[i].RemotePort
+	for i, _ := range c.ClientMappings {
+		var tcpcnf config.TCPProxyConf
+		tcpcnf.LocalIP = c.ClientMappings[i].LocalIP
+		tcpcnf.LocalPort = c.ClientMappings[i].LocalPort
+		tcpcnf.RemotePort = c.ClientMappings[i].RemotePort
 
-        proxyConfs[tcpcnf.ProxyName] = &tcpcnf
-    }
+		proxyConfs[tcpcnf.ProxyName] = &tcpcnf
+	}
 
-    cli, err := client.NewService(cfg, proxyConfs, visitorCfgs, "")
-    if err != nil {
-        return err
-    }
+	cli, err := client.NewService(cfg, proxyConfs, visitorCfgs, "")
+	if err != nil {
+		return err
+	}
 
-    cli.Run()
+	cli.Run()
 
-    return nil
+	return nil
 }
 
 // helper function to generate random
 // number in a certain range
 func rangeIn(low, hi int) int {
-    return low + rand.Intn(hi-low)
+	return low + rand.Intn(hi-low)
 }
--- p2p/speedtest.go
+++ p2p/speedtest.go
@@ -3,106 +3,106 @@ package p2p
 // SpeedTest Runs a speed test and does updates IP tables accordingly
 func (ip *IpAddresses) SpeedTest() error {
 
-    //temp variable to store elements IP addresses and other information
-    // of IP addresses that are pingable
-    var ActiveIP IpAddresses
+	//temp variable to store elements IP addresses and other information
+	// of IP addresses that are pingable
+	var ActiveIP IpAddresses
 
-    // Index to remove from struct
-    for _, value := range ip.IpAddress {
+	// Index to remove from struct
+	for _, value := range ip.IpAddress {
 
-        var err error
-        //if len(ip.IpAddress) == 1 {
-        //	i = 0
-        //}
+		var err error
+		//if len(ip.IpAddress) == 1 {
+		//	i = 0
+		//}
 
-        // Ping Test
-        err = value.PingTest()
+		// Ping Test
+		err = value.PingTest()
 
-        if err != nil {
-            continue
-        }
+		if err != nil {
+			continue
+		}
 
-        //Upload Speed Test
-        //err = value.UploadSpeed()
-        //if err != nil {
-        //	return err
-        //}
-        //
-        //err = value.DownloadSpeed()
-        //if err != nil {
-        //	return err
-        //}
+		//Upload Speed Test
+		//err = value.UploadSpeed()
+		//if err != nil {
+		//	return err
+		//}
+		//
+		//err = value.DownloadSpeed()
+		//if err != nil {
+		//	return err
+		//}
 
-        //Set value to the list
+		//Set value to the list
 
-        ActiveIP.IpAddress = append(ActiveIP.IpAddress, value)
+		ActiveIP.IpAddress = append(ActiveIP.IpAddress, value)
 
-    }
+	}
 
-    ip.IpAddress = ActiveIP.IpAddress
+	ip.IpAddress = ActiveIP.IpAddress
 
-    err := ip.WriteIpTable()
-    if err != nil {
-        return err
-    }
+	err := ip.WriteIpTable()
+	if err != nil {
+		return err
+	}
 
-    return nil
+	return nil
 }
 
 // SpeedTestUpdatedIPTable Called when ip tables from httpclient/server is also passed on
 func (ip *IpAddresses) SpeedTestUpdatedIPTable() error {
-    targets, err := ReadIpTable()
-    if err != nil {
-        return err
-    }
+	targets, err := ReadIpTable()
+	if err != nil {
+		return err
+	}
 
-    // To ensure struct has no duplicates IP addresses
-    //DoNotRead := targets
+	// To ensure struct has no duplicates IP addresses
+	//DoNotRead := targets
 
-    // Appends all IP addresses
-    for i, _ := range targets.IpAddress {
+	// Appends all IP addresses
+	for i, _ := range targets.IpAddress {
 
-        // To ensure that there are no duplicate IP addresses
-        //Exists := false
-        //for k := range ip.IpAddress {
-        //	// Checks if both the IPV4 addresses are the same or the IPV6 address is not
-        //	// an empty string and IPV6 address are the same
-        //	if ip.IpAddress[k].Ipv4 == targets.IpAddress[i].Ipv4 || (targets.IpAddress[i].Ipv6 != "" && ip.IpAddress[k].Ipv6 == targets.IpAddress[i].Ipv6) {
-        //		Exists = true
-        //		break
-        //	}
-        //}
-        //
-        //// If the struct exists then continues
-        //if Exists {
-        //	continue
-        //}
+		//To ensure that there are no duplicate IP addresses
+		Exists := false
+		for k := range ip.IpAddress {
+			// Checks if both the IPV4 addresses are the same or the IPV6 address is not
+			// an empty string and IPV6 address are the same
+			if (ip.IpAddress[k].Ipv4 == targets.IpAddress[i].Ipv4 && targets.IpAddress[i].ProxyPort == "0") || (targets.IpAddress[i].Ipv6 != "" && ip.IpAddress[k].Ipv6 == targets.IpAddress[i].Ipv6 && targets.IpAddress[i].ProxyPort == "0") {
+				Exists = true
+				break
+			}
+		}
 
-        ip.IpAddress = append(ip.IpAddress, targets.IpAddress[i])
-    }
+		// If the struct exists then continues
+		if Exists {
+			continue
+		}
 
-    err = ip.SpeedTest()
+		ip.IpAddress = append(ip.IpAddress, targets.IpAddress[i])
+	}
 
-    if err != nil {
-        return err
-    }
+	err = ip.SpeedTest()
 
-    return nil
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // LocalSpeedTestIpTable Runs speed test in iptables locally only
 func LocalSpeedTestIpTable() error {
-    targets, err := ReadIpTable()
-    if err != nil {
-        return err
-    }
+	targets, err := ReadIpTable()
+	if err != nil {
+		return err
+	}
 
-    err = targets.SpeedTest()
-    if err != nil {
-        return err
-    }
+	err = targets.SpeedTest()
+	if err != nil {
+		return err
+	}
 
-    return nil
+	return nil
 }
 
 // Helper function to remove element from an array of a struct
