69c0aa5cb17cb812f1caaad2e1496ece81cae9b0
Akilan <me@akilan.io>
2023-03-22 09:43:40 +0000

    config abstraction changes added

--- config/config.go
+++ config/config.go
@@ -1,7 +1,8 @@
 package config
 
 import (
-    "github.com/spf13/viper"
+    "encoding/json"
+    "io/ioutil"
     "os"
 )
 
@@ -28,6 +29,7 @@ type Config struct {
     GroupTrackContainersPath string
     FRPServerPort            string
     BehindNAT                string
+    CustomConfig             interface{}
     //NetworkInterface  string
     //NetworkInterfaceIPV6Index int
 }
@@ -41,6 +43,9 @@ func GetPathP2PRC(Envname string) (string, error) {
         }
     }
     curDir := os.Getenv(defaultEnvName)
+    if curDir == "" {
+        return curDir, nil
+    }
     return curDir + "/", nil
 }
 
@@ -59,45 +64,93 @@ func GetEnvName() string {
 }
 
 // ConfigInit Pass environment name as an optional parameter
-func ConfigInit(defaultsParameter map[string]interface{}, envNameOptional ...string) (*Config, error) {
+func ConfigInit(defaultsParameter map[string]interface{}, CustomConfig interface{}, envNameOptional ...string) (*Config, error) {
     if len(envNameOptional) > 0 {
         defaultEnvName = envNameOptional[0]
     }
+    //
+    ////Setting current directory to default path
+    //defaultPath, err := GetPathP2PRC(defaultEnvName)
+    //if err != nil {
+    //    return nil, err
+    //}
+    ////Paths to search for config file
+    //configPaths = append(configPaths, defaultPath)
+    //
+    ////Add all possible configurations paths
+    //for _, v := range configPaths {
+    //    viper.AddConfigPath(v)
+    //}
+    //
+    ////Read config file
+    //if err := viper.ReadInConfig(); err != nil {
+    //    // If the error thrown is config file not found
+    //    //Sets default configuration to viper
+    //    for k, v := range defaults {
+    //        viper.SetDefault(k, v)
+    //    }
+    //    viper.SetConfigName(configName)
+    //    viper.SetConfigFile(configFile)
+    //    viper.SetConfigType(configType)
+    //
+    //    if err = viper.WriteConfig(); err != nil {
+    //        return nil, err
+    //    }
+    //}
+    //
+    //// Adds configuration to the struct
+    //var config Config
+    //if err := viper.Unmarshal(&config); err != nil {
+    //    return nil, err
+    //}
+    //
+    //return &config, nil
 
-    //Setting current directory to default path
     defaultPath, err := GetPathP2PRC(defaultEnvName)
     if err != nil {
         return nil, err
     }
-    //Paths to search for config file
-    configPaths = append(configPaths, defaultPath)
 
-    //Add all possible configurations paths
-    for _, v := range configPaths {
-        viper.AddConfigPath(v)
-    }
-
-    //Read config file
-    if err := viper.ReadInConfig(); err != nil {
-        // If the error thrown is config file not found
-        //Sets default configuration to viper
-        for k, v := range defaults {
-            viper.SetDefault(k, v)
-        }
-        viper.SetConfigName(configName)
-        viper.SetConfigFile(configFile)
-        viper.SetConfigType(configType)
-
-        if err = viper.WriteConfig(); err != nil {
-            return nil, err
-        }
-    }
-
-    // Adds configuration to the struct
-    var config Config
-    if err := viper.Unmarshal(&config); err != nil {
+    // Open our jsonFile
+    jsonFile, err := os.Open(defaultPath + configFile)
+    // if we os.Open returns an error then handle it
+    if err != nil {
         return nil, err
     }
 
+    // defer the closing of our jsonFile so that we can parse it later on
+    defer jsonFile.Close()
+
+    byteValue, _ := ioutil.ReadAll(jsonFile)
+
+    var config Config
+    json.Unmarshal(byteValue, &config)
+
+    if CustomConfig != nil {
+        // Convert Custom Config to byte
+        customConfigByte, err := json.Marshal(config.CustomConfig)
+        if err != nil {
+            return nil, err
+        }
+
+        // Again map the byte to the CustomConfig interface
+        json.Unmarshal(customConfigByte, &CustomConfig)
+    }
+
     return &config, nil
+}
+
+func (c *Config) WriteConfig() error {
+    //Getting Current Directory from environment variable
+    //curDir := os.Getenv("REMOTEGAMING")
+
+    defaultPath, err := GetPathP2PRC(defaultEnvName)
+    if err != nil {
+        return err
+    }
+
+    file, _ := json.MarshalIndent(c, "", " ")
+
+    _ = ioutil.WriteFile(defaultPath+"config.json", file, 0644)
+    return nil
 }
