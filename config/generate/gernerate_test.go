ea8e7d4073f27e1ff83bb273da262c715d18aea0
xecarlox94 <jf94.uk@gmail.com>
2025-01-07 20:19:47 +0000

    uncommenting testing code

--- config/generate/gernerate_test.go
+++ config/generate/gernerate_test.go
@@ -1,40 +1,40 @@
 package generate
 
-// import (
-// 	"fmt"
-// 	"github.com/Akilan1999/p2p-rendering-computation/config"
-// 	"testing"
-// )
-//
-// type CustomConfig struct {
-// 	Test string
-// }
-//
-// // Test case to generate defaults with custom data-structure
-// func TestSetDefaults(t *testing.T) {
-// 	setDefaults, err := SetDefaults("", true, &CustomConfig{Test: "lol"}, true)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 		t.Fail()
-// 		return
-// 	}
-//
-// 	fmt.Println(setDefaults)
-//
-// 	var c CustomConfig
-//
-// 	_, err = config.ConfigInit(nil, &c)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 		t.Fail()
-// 		return
-// 	}
-//
-// 	fmt.Println(c)
-//
-// }
-//
-// // Test case to generate public and private keys
-// func TestGeneratePublicAndPrivateKeys(t *testing.T) {
-// 	MakeSSHKeyPair("test.pub", "test.prv")
-// }
+import (
+	"fmt"
+	"github.com/Akilan1999/p2p-rendering-computation/config"
+	"testing"
+)
+
+type CustomConfig struct {
+	Test string
+}
+
+// Test case to generate defaults with custom data-structure
+func TestSetDefaults(t *testing.T) {
+	setDefaults, err := SetDefaults("", true, &CustomConfig{Test: "lol"}, true)
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+		return
+	}
+
+	fmt.Println(setDefaults)
+
+	var c CustomConfig
+
+	_, err = config.ConfigInit(nil, &c)
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+		return
+	}
+
+	fmt.Println(c)
+
+}
+
+// Test case to generate public and private keys
+func TestGeneratePublicAndPrivateKeys(t *testing.T) {
+	MakeSSHKeyPair("test.pub", "test.prv")
+}
--- p2p/iptable_test.go
+++ p2p/iptable_test.go
@@ -1,76 +1,77 @@
 package p2p
-// import (
-// 	"fmt"
-// 	"testing"
-// )
-//
-// func TestReadIpTable(t *testing.T) {
-// 	json, err := ReadIpTable()
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-//
-// 	err = json.WriteIpTable()
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-//
-// 	err = PrintIpTable()
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-// }
-//
-// // Testing is a IPV6 address is returned
-// func TestGetCurrentIPV6(t *testing.T) {
-// 	res, err := GetCurrentIPV6()
-//
-// 	if err != nil {
-// 		t.Error(err)
-// 	}
-//
-// 	fmt.Println(res)
-// }
-//
-// // This test ensures that the duplicate function works as intended
-// func TestIpAddresses_RemoveDuplicates(t *testing.T) {
-// 	var testduplicates IpAddresses
-// 	var duplicateaddress1 IpAddress
-// 	var duplicateaddress2 IpAddress
-//
-// 	duplicateaddress1.Ipv6="2001:8f8:172d:ee93:7588:ad57:c351:3309"
-// 	duplicateaddress1.Ipv4="0.0.0.0"
-//
-// 	duplicateaddress2.Ipv6="2001:8f8:172d:ee93:7588:ad57:c351:3309"
-// 	duplicateaddress2.Ipv4="0.0.0.0"
-//
-// 	testduplicates.IpAddress = append(testduplicates.IpAddress, duplicateaddress1)
-// 	testduplicates.IpAddress = append(testduplicates.IpAddress, duplicateaddress2)
-//
-// 	err := testduplicates.RemoveDuplicates()
-// 	if err != nil {
-// 		t.Error(err)
-// 	}
-//
-// 	if len(testduplicates.IpAddress) == 2 {
-// 		t.Fail()
-// 	}
-//
-// }
-//
-// func TestViewNetworkInterface(t *testing.T) {
-// 	err := ViewNetworkInterface()
-// 	if err != nil {
-// 		t.Error()
-// 	}
-// }
-//
-// func TestIp4or6(t *testing.T) {
-// 	// This test ensures that the ipv6 address gets detected
-// 	test := "2001:8f8:172d:7e27:4f23:ae4:bce5:e037"
-// 	res := Ip4or6(test)
-// 	if res != "version 6" {
-// 		t.Fail()
-// 	}
-//
-// }
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestReadIpTable(t *testing.T) {
+	json, err := ReadIpTable()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = json.WriteIpTable()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = PrintIpTable()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+// Testing is a IPV6 address is returned
+func TestGetCurrentIPV6(t *testing.T) {
+	res, err := GetCurrentIPV6()
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	fmt.Println(res)
+}
+
+// This test ensures that the duplicate function works as intended
+func TestIpAddresses_RemoveDuplicates(t *testing.T) {
+	var testduplicates IpAddresses
+	var duplicateaddress1 IpAddress
+	var duplicateaddress2 IpAddress
+
+	duplicateaddress1.Ipv6="2001:8f8:172d:ee93:7588:ad57:c351:3309"
+	duplicateaddress1.Ipv4="0.0.0.0"
+
+	duplicateaddress2.Ipv6="2001:8f8:172d:ee93:7588:ad57:c351:3309"
+	duplicateaddress2.Ipv4="0.0.0.0"
+
+	testduplicates.IpAddress = append(testduplicates.IpAddress, duplicateaddress1)
+	testduplicates.IpAddress = append(testduplicates.IpAddress, duplicateaddress2)
+
+	err := testduplicates.RemoveDuplicates()
+	if err != nil {
+		t.Error(err)
+	}
+
+	if len(testduplicates.IpAddress) == 2 {
+		t.Fail()
+	}
+
+}
+
+func TestViewNetworkInterface(t *testing.T) {
+	err := ViewNetworkInterface()
+	if err != nil {
+		t.Error()
+	}
+}
+
+func TestIp4or6(t *testing.T) {
+	// This test ensures that the ipv6 address gets detected
+	test := "2001:8f8:172d:7e27:4f23:ae4:bce5:e037"
+	res := Ip4or6(test)
+	if res != "version 6" {
+		t.Fail()
+	}
+
+}
