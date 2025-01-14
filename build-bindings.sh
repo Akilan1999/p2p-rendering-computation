f9b2fa6a5da160345ee4431dfa5d1b0cb2a0faf5
Akilan <me@akilan.io>
2025-01-14 21:00:25 +0000

    Testing bindings

--- build-bindings.sh
+++ build-bindings.sh
@@ -1,1 +1,33 @@
-cd Bindings && go build -buildmode=c-shared -o p2prc.so
+cd Bindings 
+go build -buildmode=c-shared -o p2prc.so
+# mkdir p2prc-mac-amd64 
+# CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build -buildmode=c-shared -o p2prc-mac-amd64/p2prc.so
+# mkdir p2prc-mac-arm64 
+# CGO_ENABLED=1 GOOS=darwin GOARCH=arm64 go build -buildmode=c-shared -o p2prc-mac-arm64/p2prc.so
+# mkdir p2prc-windows-arm64 
+# CGO_ENABLED=1 GOOS=windows GOARCH=arm64 go build -buildmode=c-shared -o p2prc-windows-arm64/p2prc.so
+# mkdir p2prc-windows-amd64 
+# CGO_ENABLED=1 GOOS=windows GOARCH=amd64 go build -buildmode=c-shared -o p2prc-windows-amd64/p2prc.so
+# mkdir p2prc-windows-amd64 
+# CGO_ENABLED=1 GOOS=windows GOARCH=amd64 go build -buildmode=c-shared -o p2prc-windows-amd64/p2prc.so
+# mkdir p2prc-windows-arm
+# CGO_ENABLED=1 GOOS=windows GOARCH=arm go build -buildmode=c-shared -o p2prc-windows-arm/p2prc.so
+# mkdir p2prc-windows-arm
+# CGO_ENABLED=1 GOOS=windows GOARCH=arm go build -buildmode=c-shared -o p2prc-windows-arm/p2prc.so
+# mkdir p2prc-linux-386
+# CGO_ENABLED=1 GOOS=linux GOARCH=386 go build -buildmode=c-shared -o p2prc-linux-386/p2prc.so
+
+# linux/386
+# linux/amd64
+# linux/arm
+# linux/arm64
+# linux/loong64
+# linux/mips
+# linux/mips64
+# linux/mips64le
+# linux/mipsle
+# linux/ppc64
+# linux/ppc64le
+# linux/riscv64
+# linux/s390x
+
