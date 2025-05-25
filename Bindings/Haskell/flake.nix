8a2dfb7b5b423245974168db806b88e1241b8a23
xecarlox94 <jf94.uk@gmail.com>
2025-05-26 00:15:36 +0100

    fixed haskell development environment

--- Bindings/Haskell/flake.nix
+++ Bindings/Haskell/flake.nix
@@ -2,29 +2,27 @@
   description = "Nix flake for P2PRC Haskell library";
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    p2prc-main = "path:../../";
+    p2prc-main.url = "path:../../";
   };
 
-  outputs =
-    { nixpkgs, flake-utils, p2prc-main-go, ... }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        {
-          packages.default = pkgs.haskellPackages.callPackage ./project.nix {};
+  outputs = { nixpkgs, flake-utils, p2prc-main, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = pkgs.haskellPackages.callPackage ./project.nix {};
 
-          devShell = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              cabal2nix
-              cabal-install
-              haskell.compiler.ghc98
-              zlib.dev
-              p2prc-main-go.packages.${system}.default
-            ];
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            cabal2nix
+            cabal-install
+            haskell.compiler.ghc98
+            zlib.dev
+            p2prc-main.packages.${system}.default
+          ];
 
-          };
-        }
+        };
+      }
     );
 }
