0f4f52cb8b8a1c85588f619cbb96fe10d88e74e5
xecarlox94 <jf94.uk@gmail.com>
2025-05-26 00:06:11 +0100

    working on haskell development shell

--- flake.nix
+++ flake.nix
@@ -4,7 +4,11 @@
   inputs = {
 
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     gomod2nix = {
       url = "github:nix-community/gomod2nix";
