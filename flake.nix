4e7048484b865a7ffdf2a6d4bbcdc57a4eb91703
xecarlox94 <jf94.uk@gmail.com>
2025-06-02 00:31:29 +0100

    finishing automated setup

--- flake.nix
+++ flake.nix
@@ -117,7 +117,7 @@
 
               sed -i 's/base.*$/base, p2prc/' "$PROJECT_DIR".cabal
 
-              cat ${mainFileContent} > app/main.hs
+              cat ${mainFileContent} > app/Main.hs
 
               cabal2nix . > ./cabal.nix;
               cabal2nix . --shell > shell.nix
