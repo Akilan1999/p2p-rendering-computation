4c8acd8288cc7d78fc059174d6d3d31762607713
Akilan <me@akilan.io>
2025-06-04 15:51:07 +0100

    fixed pass through custom information

--- client/clientIPTable/AddCustomInformationToIPTable.go
+++ client/clientIPTable/AddCustomInformationToIPTable.go
@@ -1,40 +1,40 @@
 package clientIPTable
 
 import (
-	"errors"
-	"github.com/Akilan1999/p2p-rendering-computation/config"
-	"github.com/Akilan1999/p2p-rendering-computation/p2p"
+    "errors"
+    "github.com/Akilan1999/p2p-rendering-computation/config"
+    "github.com/Akilan1999/p2p-rendering-computation/p2p"
 )
 
 func AddCustomInformationToIPTable(text string) error {
-	// Get config information
-	Config, err := config.ConfigInit(nil, nil)
-	if err != nil {
-		return err
-	}
+    // Get config information
+    Config, err := config.ConfigInit(nil, nil)
+    if err != nil {
+        return err
+    }
 
-	// Get IPTable information
-	table, err := p2p.ReadIpTable()
-	if err != nil {
-		return err
-	}
+    // Get IPTable information
+    table, err := p2p.ReadIpTable()
+    if err != nil {
+        return err
+    }
 
-	found := false
+    found := false
 
-	for i, _ := range table.IpAddress {
-		if table.IpAddress[i].Name == Config.MachineName {
-			table.IpAddress[i].CustomInformation = text
-			found = true
-		}
-	}
+    for i, _ := range table.IpAddress {
+        if table.IpAddress[i].Name == Config.MachineName {
+            table.IpAddress[i].CustomInformation = text
+            found = true
+        }
+    }
 
-	if found {
-		table.WriteIpTable()
-		// update IPTable after modified entry
-		go UpdateIpTableListClient()
-	} else {
-		return errors.New("start server with p2prc -s as the server is currently not running")
-	}
+    if found {
+        table.WriteIpTable()
+        // update IPTable after modified entry
+        UpdateIpTableListClient()
+    } else {
+        return errors.New("start server with p2prc -s as the server is currently not running")
+    }
 
-	return nil
+    return nil
 }
--- p2p/speedtest.go
+++ p2p/speedtest.go
@@ -1,136 +1,137 @@
 package p2p
 
 import (
-	"github.com/Akilan1999/p2p-rendering-computation/config"
+    "github.com/Akilan1999/p2p-rendering-computation/config"
 )
 
 // SpeedTest Runs a speed test and does updates IP tables accordingly
 func (ip *IpAddresses) SpeedTest() error {
 
-	//temp variable to store elements IP addresses and other information
-	// of IP addresses that are pingable
-	var ActiveIP IpAddresses
+    //temp variable to store elements IP addresses and other information
+    // of IP addresses that are pingable
+    var ActiveIP IpAddresses
 
-	// Index to remove from struct
-	for _, value := range ip.IpAddress {
+    // Index to remove from struct
+    for _, value := range ip.IpAddress {
 
-		var err error
-		//if len(ip.IpAddress) == 1 {
-		//	i = 0
-		//}
+        var err error
+        //if len(ip.IpAddress) == 1 {
+        //	i = 0
+        //}
 
-		// Ping Test
-		err = value.PingTest()
+        // Ping Test
+        err = value.PingTest()
 
-		if err != nil {
-			continue
-		}
+        if err != nil {
+            continue
+        }
 
-		//Upload Speed Test
-		//err = value.UploadSpeed()
-		//if err != nil {
-		//	return err
-		//}
-		//
-		//err = value.DownloadSpeed()
-		//if err != nil {
-		//	return err
-		//}
+        //Upload Speed Test
+        //err = value.UploadSpeed()
+        //if err != nil {
+        //	return err
+        //}
+        //
+        //err = value.DownloadSpeed()
+        //if err != nil {
+        //	return err
+        //}
 
-		//Set value to the list
+        //Set value to the list
 
-		ActiveIP.IpAddress = append(ActiveIP.IpAddress, value)
-	}
+        ActiveIP.IpAddress = append(ActiveIP.IpAddress, value)
+    }
 
-	ip.IpAddress = ActiveIP.IpAddress
+    ip.IpAddress = ActiveIP.IpAddress
 
-	err := ip.WriteIpTable()
-	if err != nil {
-		return err
-	}
+    err := ip.WriteIpTable()
+    if err != nil {
+        return err
+    }
 
-	return nil
+    return nil
 }
 
 // SpeedTestUpdatedIPTable Called when ip tables from httpclient/server is also passed on
 func (ip *IpAddresses) SpeedTestUpdatedIPTable() error {
 
-	Config, err := config.ConfigInit(nil, nil)
-	if err != nil {
-		return err
-	}
+    Config, err := config.ConfigInit(nil, nil)
+    if err != nil {
+        return err
+    }
 
-	targets, err := ReadIpTable()
-	if err != nil {
-		return err
-	}
+    targets, err := ReadIpTable()
+    if err != nil {
+        return err
+    }
 
-	// Checks if baremetal mode and unsafe mode
-	// is enabled. If it is enabled it adds the
-	// the propagated public key to the list.
+    // Checks if baremetal mode and unsafe mode
+    // is enabled. If it is enabled it adds the
+    // the propagated public key to the list.
 
-	AddPublicKey := false
-	if Config.BareMetal && Config.UnsafeMode {
-		AddPublicKey = true
-	}
+    AddPublicKey := false
+    if Config.BareMetal && Config.UnsafeMode {
+        AddPublicKey = true
+    }
 
-	// To ensure struct has no duplicates IP addresses
-	//DoNotRead := targets
+    // To ensure struct has no duplicates IP addresses
+    //DoNotRead := targets
 
-	// Appends all IP addresses
-	for i, _ := range targets.IpAddress {
+    // Appends all IP addresses
+    for i, _ := range targets.IpAddress {
 
-		//To ensure that there are no duplicate IP addresses
-		Exists := false
-		for k := range ip.IpAddress {
-			if AddPublicKey && ip.IpAddress[k].PublicKey != "" {
-				// This function call (AddAuthorisationKey) is inefficient but to be optimised later on.
-				// This is because when if the user is running on as unsafe mode the authorization file
-				// is opened from the SSH directory and then iterates through every single SSH entry
-				// to find out if the SSH entry exists or not. This will incur multiple CPU cycles
-				// for no reason. A better approach would be to have been to store the states on memory and only
-				// add when needed based on the memory location. This is something is to be discussed
-				// and look upon later on.
-				AddAuthorisationKey(ip.IpAddress[k].PublicKey)
-			}
-			// Checks if both the IPV4 addresses are the same or the IPV6 address is not
-			// an empty string and IPV6 address are the same
-			if (ip.IpAddress[k].Ipv4 == targets.IpAddress[i].Ipv4 && targets.IpAddress[i].NAT) || (targets.IpAddress[i].Ipv6 != "" && ip.IpAddress[k].Ipv6 == targets.IpAddress[i].Ipv6) {
-				Exists = true
-				break
-			}
-		}
+        //To ensure that there are no duplicate IP addresses
+        Exists := false
+        for k := range ip.IpAddress {
+            if AddPublicKey && ip.IpAddress[k].PublicKey != "" {
+                // This function call (AddAuthorisationKey) is inefficient but to be optimised later on.
+                // This is because when if the user is running on as unsafe mode the authorization file
+                // is opened from the SSH directory and then iterates through every single SSH entry
+                // to find out if the SSH entry exists or not. This will incur multiple CPU cycles
+                // for no reason. A better approach would be to have been to store the states on memory and only
+                // add when needed based on the memory location. This is something is to be discussed
+                // and look upon later on.
+                AddAuthorisationKey(ip.IpAddress[k].PublicKey)
+            }
+            targets.IpAddress[i].CustomInformation = ip.IpAddress[k].CustomInformation
+            // Checks if both the IPV4 addresses are the same or the IPV6 address is not
+            // an empty string and IPV6 address are the same
+            if (ip.IpAddress[k].Ipv4 == targets.IpAddress[i].Ipv4 && targets.IpAddress[i].NAT) || (targets.IpAddress[i].Ipv6 != "" && ip.IpAddress[k].Ipv6 == targets.IpAddress[i].Ipv6) {
+                Exists = true
+                break
+            }
+        }
 
-		// If the struct exists then continues
-		if Exists {
-			continue
-		}
+        // If the struct exists then continues
+        if Exists {
+            continue
+        }
 
-		ip.IpAddress = append(ip.IpAddress, targets.IpAddress[i])
-	}
+        ip.IpAddress = append(ip.IpAddress, targets.IpAddress[i])
+    }
 
-	err = ip.SpeedTest()
+    err = ip.SpeedTest()
 
-	if err != nil {
-		return err
-	}
+    if err != nil {
+        return err
+    }
 
-	return nil
+    return nil
 }
 
 // LocalSpeedTestIpTable Runs speed test in iptables locally only
 func LocalSpeedTestIpTable() error {
-	targets, err := ReadIpTable()
-	if err != nil {
-		return err
-	}
+    targets, err := ReadIpTable()
+    if err != nil {
+        return err
+    }
 
-	err = targets.SpeedTest()
-	if err != nil {
-		return err
-	}
+    err = targets.SpeedTest()
+    if err != nil {
+        return err
+    }
 
-	return nil
+    return nil
 }
 
 // Helper function to remove element from an array of a struct
