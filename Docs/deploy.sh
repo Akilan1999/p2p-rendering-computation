3e547457f2f881fa127e1e54b7aa516b0a900f87
Akilan <me@akilan.io>
2026-08-04 12:28:52 +0100

    fixed docs

---
+++ Docs/deploy.sh
@@ -0,0 +1,5 @@
+rsync -ru index.html home-1:/home/akilan/Documents/p2prc/docs/
+
+rsync -ru style.css home-1:/home/akilan/Documents/p2prc/docs/
+
+rsync -ru Colored-On-Light-Image.png home-1:/home/akilan/Documents/p2prc/docs/
