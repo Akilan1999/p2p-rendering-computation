f9494753f60f5cd8b62570d9501d669f7e652c59
Akilan <me@akilan.io>
2026-08-07 10:09:14 +0100

    disabled CRON

--- server/cron.go
+++ server/cron.go
@@ -1,45 +1,45 @@
 package server
 
 import (
-	"fmt"
-	"github.com/Akilan1999/p2p-rendering-computation/config"
-	"github.com/Akilan1999/p2p-rendering-computation/p2p"
-	"github.com/go-co-op/gocron/v2"
-	"time"
+    "fmt"
+    "github.com/Akilan1999/p2p-rendering-computation/config"
+    "github.com/Akilan1999/p2p-rendering-computation/p2p"
+    "github.com/go-co-op/gocron/v2"
+    "time"
 )
 
 func CRON() {
-	s, _ := gocron.NewScheduler()
+    s, _ := gocron.NewScheduler()
 
-	s.NewJob(
-		gocron.DurationJob(
-			20*time.Second,
-		),
-		gocron.NewTask(
-			func() {
-				//go clientIPTable.RemoveOfflineNodes()
-				go RestartNATIfServerNotFound()
-			},
-		),
-	)
+    s.NewJob(
+        gocron.DurationJob(
+            20*time.Second,
+        ),
+        gocron.NewTask(
+            func() {
+                //go clientIPTable.RemoveOfflineNodes()
+                // go RestartNATIfServerNotFound()
+            },
+        ),
+    )
 
-	s.Start()
+    s.Start()
 
 }
 
 // RestartNATIfServerNotFound The aim is to attempt to restart the server
 // if it's not reachable from the outside.
 func RestartNATIfServerNotFound() {
-	config, err := config.ConfigInit(nil, nil)
-	if err != nil {
-		return
-	}
-	_, err = p2p.SearchMachine(config.MachineName)
-	if err != nil {
-		fmt.Println("Server attempting to restart again since it's not pingable")
-		// Increasing the wait time for the NAT traversal
-		NATEscapeTime += 1
-		SetupServerCurrentMachine()
-		return
-	}
+    config, err := config.ConfigInit(nil, nil)
+    if err != nil {
+        return
+    }
+    _, err = p2p.SearchMachine(config.MachineName)
+    if err != nil {
+        fmt.Println("Server attempting to restart again since it's not pingable")
+        // Increasing the wait time for the NAT traversal
+        NATEscapeTime += 1
+        SetupServerCurrentMachine()
+        return
+    }
 }
